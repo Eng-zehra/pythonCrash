daf26959db7b90cdbc6690be82708057a51c86b1
Eng-zehra <sahracabdirisaq937@gmail.com>
2025-10-02 21:41:40 +0300

    Meaninfuling the else statement

--- guesting.py
+++ guesting.py
@@ -1,10 +1,10 @@
 import random
 number=random.randint(0,10)
-guest=int(input("Enter Your guesting number: "))
-if(guest==number):
-    print("Congratulations you got it " ,guest)
-elif(guest<number):
-    print(f"Guess Too Low   {guest}")
+guest=int(input("Enter your guessing number (0-10):"))
+if(guest < 0 or guest >10):
+    print("Invalid guess! Please choose a number between 0 and 10.")
+elif(guest==number):
+        print("Congratulations you got it " ,guest)
 elif(guest>number):
-    print(f"Guess too hight {guest}")
-else:print("opps its not")
+        print(f"Guess too hight {guest}")
+else:print("Guess Too low  ")
