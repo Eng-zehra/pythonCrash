f44686a992523f8cf7b3bb6f5ef09bc60b573eee
Eng-zehra <sahracabdirisaq937@gmail.com>
2025-10-03 22:24:50 +0300

    no

--- conditional.py
+++ conditional.py
@@ -1,49 +1,49 @@
 # # 
-# number=int(input("Enter number To Check if its Possitive or Negative "))
-# if(number==0):
-#     print(number,"Is Zero")
-# elif(number>0):
-#     print(number,"Is Positive number")
-# else:
-#     print(number,"IS Negative ")
+number=int(input("Enter number To Check if its Possitive or Negative "))
+if(number==0):
+    print(number,"Is Zero")
+elif(number>0):
+    print(number,"Is Positive number")
+else:
+    print(number,"IS Negative ")
 
 # #legible age
-# age=int(input("Enter Your Age "))
-# if(age>=18):
-#     print("Eligible to vote")
-# else:print("Not eligible")
-# #discount
-# bill=int(input("Bill amount: "))
-# if(bill>=1000):
-#     discount=bill*0.2
-#     result=bill-discount
-# elif(bill>=500):
-#     discount=bill*0.2
-#     result=bill-discount
-# else:
-#     discount=bill*0.2
-#     result=bill-discount
-# print("Final amount after discount:", result)
+age=int(input("Enter Your Age "))
+if(age>=18):
+    print("Eligible to vote")
+else:print("Not eligible")
+#discount
+bill=int(input("Bill amount: "))
+if(bill>=1000):
+    discount=bill*0.2
+    result=bill-discount
+elif(bill>=500):
+    discount=bill*0.2
+    result=bill-discount
+else:
+    discount=bill*0.2
+    result=bill-discount
+print("Final amount after discount:", result)
 
 # #Tricky
 
-# num1=int(input("Enter number 1 "))
-# num2=int(input("Enter number 2 "))
-# num3=int(input("Enter number 3 "))
-# if(num1>=num2 and num1>=num3):
-#     print(num1,"One Is The Max")
-# elif(num2>=num1 and num2>=num3):
-#     print(num2,"Two IS THE MAX")
-# elif(num3>=num2 and num3>=num1):
-#     print(num3,"Three IS The Max")
-# else:
-#     print("All numbers are Same")
+num1=int(input("Enter number 1 "))
+num2=int(input("Enter number 2 "))
+num3=int(input("Enter number 3 "))
+if(num1>=num2 and num1>=num3):
+    print(num1,"One Is The Max")
+elif(num2>=num1 and num2>=num3):
+    print(num2,"Two IS THE MAX")
+elif(num3>=num2 and num3>=num1):
+    print(num3,"Three IS The Max")
+else:
+    print("All numbers are Same")
 
 # #divisble 2 and 4
-# num=int(input("Enter a number: "))
-# if(num %2==0 and num %4==0):
-#     print(num ,"is divible by 2 and 4")
-# else:print(num,"is not divisible by 2 and 4")
+num=int(input("Enter a number: "))
+if(num %2==0 and num %4==0):
+    print(num ,"is divible by 2 and 4")
+else:print(num,"is not divisible by 2 and 4")
 
 year=int(input("Enter a year leap or not"))
 if(year % 400==0 ):
@@ -90,5 +90,4 @@ elif(lenth1==lenth2 or lenth1==lenth3 or lenth2==lenth3):
 else:
     print("Scalene")
 #password cheker
-password=int(input("Enter your password: "))
-len(password)>8 this looks hard
+
